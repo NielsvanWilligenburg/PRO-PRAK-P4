5d1cad2054b3fc9de699b634badb5df6e85051be
Neles-333323 <333323@student.mboutrecht.nl>
2022-05-25 16:44:51 +0200

    custom post-type af alleen nog de styling ervan

--- wp-content/themes/bread/single-reviews.php
+++ wp-content/themes/bread/single-reviews.php
@@ -1,16 +1,24 @@
 <?php
+
 /** @var string $title */
 /** @var string $content */
 
-var_dump('hier ben ik');
+$title = get_the_title();
+$content = get_the_content();
+$score = get_field('score');
+$image = get_field('image', get_post()->ID);
+$url = get_field('url');
 ?>
 
 
+
+
 <div class="card">
-    <?php if($title): ?>
-        <h1><?php echo $title; ?></h1>
-    <?php endif; ?>
-    <?php if($content): ?>
-        <?php echo $content; ?>
-    <?php endif; ?>
-</div>
+    <h1><?php echo $title ?></h1>
+    
+    <img src=<?php echo $image['url']; ?> alt="">
+    <p><?php echo $content ?></p>
+    <div class="line">
+        <p><?php echo $score ?></p>
+        <p><?php echo $url ?></p>
+    </div>
