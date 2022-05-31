27e6337ff71f0c1cb052166c5c1e86ded77192ab
Neles-333323 <333323@student.mboutrecht.nl>
2022-05-31 16:39:17 +0200

    Styling toegevoegd

--- wp-content/themes/bread/lib/actions/random_menu_item.php
+++ wp-content/themes/bread/lib/actions/random_menu_item.php
@@ -3,20 +3,20 @@
 add_action('admin_menu', 'random_menu_page');
 
 function random_menu_page(){
-    add_menu_page('Brood', 'Brood!', 'manage_options', 'dough', 'page_render', 'dashicons-smiley');
+    add_menu_page('Wonder reviews', 'Wonder reviews!', 'manage_options', 'dough', 'page_render', 'dashicons-smiley');
 }
 
 
 function page_render(){
     echo '
-    <h1>Brood:</h1>
-    <p>Brood is erg lekker</p>
+    <h1></h1>
+    <p></p>
     
     <br>
     
-    <h2>Maar waarom Brood?</h2>
-    <p>Het is erg knapperig en erg lekker, een banaan op je brood kan ook.</p>
+    <h2>Welkom bij Wonder reviews!</h2>
+    <p></p>
     
-    <img class="temp" src="'. get_stylesheet_directory_uri() . '/assets/img/brood.jpg' . '" style="width: 25%">
+    <img class="temp" src="'. get_stylesheet_directory_uri() . '' . '" style="width: 25%">
     ';
 }
