17cf6cd2d8d4e043e9ad2f753c5289a2b834e978
Bouyaghjdane <332766@student.mboutrecht.nl>
2022-06-01 13:48:09 +0100

    custom fields op page
    filpmje van wob en sander gevolgd en toegepast alleen nu nog de IMG toevoegen

--- wp-content/themes/bread/front-page.php
+++ wp-content/themes/bread/front-page.php
@@ -4,6 +4,7 @@
 $title = get_the_title();
 $content = get_the_content();
 
+
 $arguments = ['post_type' => 'reviews', 'numberposts' => 4, 'category' => 0, 'orderby' => 'date', 'order' => 'DESC'];
 $listOfRieviews = get_posts($arguments);
 
@@ -16,6 +17,14 @@ render('views/templates/home.php', ['title' => $title, 'content' => $content]);
 
 <ul>
     <?php foreach($listOfRieviews as $review): ?>
-    <li><?php echo $review->post_title; ?></li>
+    <div class="card">
+    <h1><?php echo $title ?></h1>
+    
+    <img src=<?php echo $image['url']; ?> alt="">
+    <p><?php echo $content ?></p>
+    <div class="line">
+        <p><?php echo $score ?></p>
+        <p><a href="<?php echo get_field('url', $review->ID) ?>">Klik hier voor een website</a></p>
+    </div>
     <?php endforeach ?>
 </ul>
