e7a17daddf2d343ea6fb3ed796e64024ec3d4440
Neles-333323 <333323@student.mboutrecht.nl>
2022-06-01 15:54:44 +0200

    custom post type op de pagina gedaan met de styling ervan aangepast

--- wp-content/themes/bread/front-page.php
+++ wp-content/themes/bread/front-page.php
@@ -6,7 +6,7 @@
 
 $title = get_the_title();
 $content = get_the_content();
-$score = get_field('score');
+$score = get_field('url', $review->ID);
 $image = get_field('image', get_post()->ID);
 $url = get_field('url', $review->ID);
 
@@ -23,13 +23,23 @@ render('views/templates/home.php', ['title' => $title, 'content' => $content]);
 
     <?php foreach($listOfRieviews as $review): ?>
         <div class="card">
-    <h1><?php echo $title ?></h1>
+    <h1><?php echo $review->post_title; ?></h1>
+    <?php
     
-    <img src=<?php echo $image['url']; ?> alt="">
-    <p><?php echo $content ?></p>
+    $post = get_posts($arguments['post_title']);
+    $posts = get_posts($arguments);
+
+    // var_dump($posts);
+    // echo $post;
+
+    
+
+        ?>
+    <img src=<?php echo get_field('image', $review->ID)['url'] ?> alt="">
+    <p><?php echo $review->post_content; ?></p>
     <div class="line">
-        <p><?php echo $score ?></p>
-        <p><a href="<?php echo get_field('url', $review->ID) ?>">Klik hier voor een website</a></p>
+        <p><?php echo get_field('score', $review->ID) ?></p>
+        <p><a href="<?php echo get_field('url', $review->ID) ?>">Klik hier</a></p>
     </div>
     <?php endforeach ?>
 </ul>
