0a546f5ca73e6f0982426d3979510259859e06f8
Neles-333323 <333323@student.mboutrecht.nl>
2022-06-01 16:24:53 +0200

    Styling aangepast van de cards

--- wp-content/themes/bread/front-page.php
+++ wp-content/themes/bread/front-page.php
@@ -1,14 +1,11 @@
 <?php
+
 /** config for home template **/
 
 /** @var string $title */
 /** @var string $content */
 
-$title = get_the_title();
-$content = get_the_content();
-$score = get_field('url', $review->ID);
-$image = get_field('image', get_post()->ID);
-$url = get_field('url', $review->ID);
+
 
 
 $arguments = ['post_type' => 'reviews', 'numberposts' => 4, 'category' => 0, 'orderby' => 'date', 'order' => 'DESC'];
@@ -21,25 +18,16 @@ render('views/templates/home.php', ['title' => $title, 'content' => $content]);
 ?>
 
 
-    <?php foreach($listOfRieviews as $review): ?>
-        <div class="card">
-    <h1><?php echo $review->post_title; ?></h1>
-    <?php
-    
-    $post = get_posts($arguments['post_title']);
-    $posts = get_posts($arguments);
-
-    // var_dump($posts);
-    // echo $post;
-
-    
-
-        ?>
-    <img src=<?php echo get_field('image', $review->ID)['url'] ?> alt="">
-    <p><?php echo $review->post_content; ?></p>
-    <div class="line">
-        <p><?php echo get_field('score', $review->ID) ?></p>
-        <p><a href="<?php echo get_field('url', $review->ID) ?>">Klik hier</a></p>
+<div class="row">
+<?php foreach ($listOfRieviews as $review) : ?>
+    <div class="card">
+        <h1><?php echo $review->post_title; ?></h1>
+        <img src=<?php echo get_field('image', $review->ID)['url'] ?> alt="">
+        <p><?php echo $review->post_content; ?></p>
+        <div class="line">
+            <p><?php echo get_field('score', $review->ID) ?></p>
+            <p><a href="<?php echo get_field('url', $review->ID) ?>">Klik hier</a></p>
+        </div>
     </div>
-    <?php endforeach ?>
-</ul>
+<?php endforeach ?>
+</div>
